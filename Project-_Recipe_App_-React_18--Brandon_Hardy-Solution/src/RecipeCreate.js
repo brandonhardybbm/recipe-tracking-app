1640b320dc559ccae42afe1b9f6a40940d6bcc03
Brandon Hardy <145735262+brandonhardybbm@users.noreply.github.com>
2024-09-13 12:31:11 -0400

    Update RecipeCreate.js

--- Project-_Recipe_App_-React_18--Brandon_Hardy-Solution/src/RecipeCreate.js
+++ Project-_Recipe_App_-React_18--Brandon_Hardy-Solution/src/RecipeCreate.js
@@ -1,17 +1,43 @@
 import React, { useState } from "react";
 
-function RecipeCreate() {
+function RecipeCreate({ createRecipe }) {
+  
+  const [name, setName] = useState("");
+  const [cuisine, setCuisine] = useState("");
+  const [photo, setPhoto] = useState("");
+  const [ingredients, setIngredients] = useState("");
+  const [preparation, setPreparation] = useState("");
 
-  // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
-  // TODO: Add the required input and textarea form elements.
-  // TODO: Add the required submit and change handlers.
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    createRecipe({name, cuisine, photo, ingredients, preparation});
+    setName("");
+    setCuisine("");
+    setPhoto("");
+    setIngredients("");
+    setPreparation("");
+  }
   
   return (
-    <form name="create">
+    <form name="create" onSubmit={handleSubmit}>
       <table>
         <tbody>
           <tr>
-            <td></td>
+            <td>
+              <input type="text" name="name" onChange={(e) => setName(e.target.value)} value={name} placeholder="Name" required={true}/>
+            </td>
+            <td>
+              <input type="text" name="cuisine" onChange={(e) => setCuisine(e.target.value)} value={cuisine} placeholder="Cuisine"/>
+            </td>
+            <td>
+              <input type="url" name="photo" onChange={(e) => setPhoto(e.target.value)} value={photo} placeholder="URL"/>
+            </td>
+            <td>
+              <textarea name="ingredients" onChange={(e) => setIngredients(e.target.value)} value={ingredients} placeholder="Ingredients"/>
+            </td>
+            <td>
+              <textarea name="preparation" onChange={(e) => setPreparation(e.target.value)} value={preparation} placeholder="Preparation"/>
+            </td>
             <td>
               <button type="submit">Create</button>
             </td>
