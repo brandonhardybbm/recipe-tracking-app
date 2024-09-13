4a11b8b293676f7412a30f349d86bf2da014f143
Brandon Hardy <145735262+brandonhardybbm@users.noreply.github.com>
2024-09-13 12:30:44 -0400

    Update App.js

--- Project-_Recipe_App_-React_18--Brandon_Hardy-Solution/src/App.js
+++ Project-_Recipe_App_-React_18--Brandon_Hardy-Solution/src/App.js
@@ -7,15 +7,19 @@ import RecipeData from "./RecipeData"
 function App() {
   const [recipes, setRecipes] = useState(RecipeData);
 
-  // TODO: Add the ability for the <RecipeList /> component to list and delete an existing recipe.
-  // TODO: Add the ability for the <RecipeCreate /> component to create new recipes.
+  const deleteRecipe = (indexToDelete) => {
+    setRecipes((currentRecipes) => (currentRecipes.filter((r, i) => i !== indexToDelete)));
+  }
 
+  const createRecipe = (recipe) => {
+    setRecipes([...recipes, recipe]);
+  }
   
   return (
     <div className="App">
       <header><h1>Delicious Food Recipes</h1></header>
-      <RecipeList />
-      <RecipeCreate />
+      <RecipeList recipes={recipes} deleteRecipe={deleteRecipe}/>
+      <RecipeCreate createRecipe={createRecipe} />
     </div>
   );
 }
